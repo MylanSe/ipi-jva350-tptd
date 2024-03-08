d87784d101717df912f7f6d1fa266d2108cc0691
Mylan Seux <mylan.seux@gmail.com>
2024-03-08 15:30:59 +0100

    testJourFeriee + testPremierJourAnneeConges

--- src/test/java/com/ipi/jva350/model/EntrepriseTest.java
+++ src/test/java/com/ipi/jva350/model/EntrepriseTest.java
@@ -1,9 +1,14 @@
 package com.ipi.jva350.model;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.cglib.core.Local;
 
 public class EntrepriseTest {
     
@@ -41,5 +46,32 @@ public class EntrepriseTest {
         boolean res = Entreprise.estDansPlage(d, debut, fin);
         // Then :
         Assertions.assertEquals(false, res, "le 1er juin n'est pas compris entre le 21 janvier 2022 et le 1er juin 2022 exclus");
+    }
+
+    @ParameterizedTest(name = "jour {0} est ferie : {1}")
+    @CsvSource({
+            "'2024-05-08', true",
+            "'2024-03-01', false",
+            "'2024-02-29', false",
+            "'2024-04-01', true"
+
+    })
+    public void TestEstJourFerie(String jour, Boolean res){
+        //Given, When, Then
+        LocalDate test = LocalDate.parse(jour);
+        Assertions.assertEquals(res, Entreprise.estJourFerie(test));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "'2023-04-01', '2022-06-01'",
+            "'2023-09-01', '2023-06-01'"
+    })
+    public void TestGetPremierJourAnneeDeConges(String d, String premierJour){
+        //Given, When, Then
+        LocalDate test = LocalDate.parse(d);
+        LocalDate attendu = LocalDate.parse(premierJour);
+        Assertions.assertEquals(Entreprise.getPremierJourAnneeDeConges(test), attendu);
     }
 }
